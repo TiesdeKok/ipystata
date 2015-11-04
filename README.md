0846a2b45af24bfbb0f73ea9acac2441048be77f
Ties de Kok <kok.ties@gmail.com>
2015-11-04 16:41:12 +0100

    IPyStata 0.2

--- README.md
+++ README.md
@@ -1,57 +1,91 @@
 # IPyStata
 
-Enables the use of Stata cell magic in Jupyter (IPython) Notebooks.
+Enables the use of Stata together with Python via Jupyter (IPython) notebooks.
 
-Allows the user to write and execute Stata code from within a Jupyer Notebook.  
-Interaction with Stata is accomplished through the batch mode of Stata. The code contained in a %%stata cell is send to Stata via a temporary ‘.do’ file, the Stata output is captured via ‘.log’ files and datasets are transferred by converting Pandas dataframes to ‘.dta’ files and vice-versa. All intermediate files are stored in the ‘.ipython/stata’ directory.
+Allows the user to write and execute Stata code from within a Jupyter (IPython) Notebook.  See the example notebook below for an illustration of the functionality. 
 
-
-**Author:**   Ties de Kok <t.c.j.dekok@tilburguniversity.edu>  
-**Twitter:** @TiesdeKok <https://twitter.com/TiesdeKok/>  
+**Author:**   Ties de Kok *(t.c.j.dekok@tilburguniversity.edu)*
+**Twitter:** [@TiesdeKok](https://twitter.com/TiesdeKok/)
 **Homepage:**    https://github.com/TiesdeKok/ipystata  
 **PyPi:** https://pypi.python.org/pypi/ipystata  
-**Documentation:** http://nbviewer.ipython.org/github/TiesdeKok/ipystata/blob/master/ipystata/Example.ipynb
+**Documentation:** [**basic instructions**](#basic-instructions) or [**example notebook**](http://nbviewer.ipython.org/github/TiesdeKok/ipystata/blob/master/ipystata/Example.ipynb)
 
-Let me know what you think!
-==============
+## Practical information:
 
-If you have tips, comments, feedback or just want to say hello, feel free to contact me via email or twitter (@TiesdeKok).
-
-Install
-=======
-
-You can install or upgrade via pip:  
+####**You can install or upgrade via pip: **
 
     pip install ipystata
+  
+####**Register your Stata instance**
+Go to your Stata installation directory and either:
 
-Dependencies
-============
+-  Shift + Right-Click --> click "*Open command window here*"
+**or** 
+- Open command window (search for "*cmd*") and type: 
+`cd C:\Program Files (x86)\Stata14`      (Obviously change it to your Stata directory)
 
+Look up the name of your Stata executable (e.g. `StataMP-64.exe`) and in your command window type:
+`StataMP-64.exe /Register`
+
+Note that this is independent of IPyStata, if you have done it before there is no need to do it again. 
+For more detailed instructions see [this page](http://www.stata.com/automation/#createmsapp).
+  
+####**Dependencies**
 IPython 3 (Not yet tested in IPython 4) (http://ipython.org/)  
-Pandas (http://pandas.pydata.org/)  
-Stata (http://www.stata.com/)
+Pandas **0.17.x +** (http://pandas.pydata.org/)  (I recommend to use a distribution like Anaconda)
+Recent version of Stata (13 / 14 preferably) (http://www.stata.com/)
+
+####**How to use?**
+Make sure that you have a registered Stata instance ([instructions](#register-your-stata-instance))!
+
+You can use IPyStata using the `%%stata` cell magic. 
+See the [**basic instructions**](#basic_instructions) below or the [**example notebook**](http://nbviewer.ipython.org/github/TiesdeKok/ipystata/blob/master/ipystata/Example.ipynb).
+
+Note that all intermediate files are stored in the ‘.ipython/stata’ directory.
+Several options are included to manage your sessions, see the [**session manager**](#session-manager) section.
+
+## What is new in 0.2:
+
+After a discussion with James Fielder I decided to overhaul my initial code to have it interact with Stata using Automation instead of the batch mode. This approach is inspired by James his Stata-Kernel, check out the awesome early development version here: https://github.com/jrfiedler/stata-kernel. 
+
+> **Pros:**
+>
+> - Extra functionality: 
+>     - Persistent Stata sessions. (*Just as-if you were using Stata directly!*)
+>     - Multiple Stata sessions in one notebook.
+>     - Allows IPystata to  retrieve macros directly from Stata into Python.
+> - This approach is more idiomatic as it allows for direct interaction with Stata.
+> - Keeps my Stata magic functionality consistent with the Stata kernel by James Fiedler. 
+> 
+> **Cons:**
+> 
+> - Windows only (Stata Automation is Windows only).
+> - Requires the user to register their Stata client.
+> - Requires recent Stata version (13 / 14).
+> 
+> **Bug fixes and other improvements:**
+> 
+> - Improved the output display functionality:
+>  - Loops should now be displayed correctly. 
+>  - Fixed inconsistent white spaces at the begin / end of output. 
+> - Internal file-handling changed to using absolute paths, working directory functionality is now explicitly included in the -cwd argument. 
+> - Package is compatible for both Python 2.7.x and Python 3.x.
+>
+> **Todo:**
+> 
+> - Compatibility with IPython 4 (Jupyter).
+> - Add an option for non-Windows users that uses the batch mode functionality.
+> - Explore the possibilities of asynchronous Stata code execution using different sessions. 
+> - Improve Stata syntax highlighting.
 
 
-Basic instructions
-==================
+## Basic instructions
 
-For an example notebook see: http://nbviewer.ipython.org/github/TiesdeKok/ipystata/blob/master/ipystata/Example.ipynb
+For an example of the functionality see the [**example notebook**](http://nbviewer.ipython.org/github/TiesdeKok/ipystata/blob/master/ipystata/Example.ipynb).
+If you have never done it before, make sure to register your Stata instance ([**instructions**](#register-your-stata-instance)).
 
-One initial configuration step is required to define your installation of Stata by running:  
-
-    In[1]: import ipystata
-    In[2]: from ipystata.ipystata_magic import iPyStata  
-    In[3]: iPyStata.config_stata('Path to your Stata executable')  
-
-For example:
-
-    In[1]: import ipystata  
-    In[2]: from ipystata.ipystata_magic import iPyStata  
-    In[3]: iPyStata.config_stata('C:\Program Files (x86)\Stata13\StataMP-64.exe')  
-
-The configuration will be saved and is thus only required to be performed once at initial use.
-
-IPyStata is imported and loaded using "import ipystata". A cell with Stata code is started with the cell magic "%%stata".
+IPyStata is imported and loaded using `import ipystata`. 
+A cell with Stata code is defined by the cell magic `%%stata`.
 
 For example:
 
@@ -60,10 +94,20 @@ For example:
            display "Hello, I am printed in Stata."  
 
 
-Arguments
-==========
+##Arguments
 
-Send a Pandas dataframe to be used in Stata:  
+Define a session to execute the code with (**New in 0.2**):
+
+    -s --session
+    In[1]: %%stata -s session_name
+(Note: if no `session` argument is provided the main session is used.)
+
+Set your Python working directory to the Stata session (**New in 0.2**):
+
+    -cwd --changewd
+    In[1]: %%stata -cwd 
+    
+Send a Pandas dataframe to be used in the Stata session:  
 
     -d --data  
     In[1]: %%stata -d dataframe  
@@ -79,19 +123,58 @@ Input Python lists and load them into Stata as macros:
     In[1]: example_list = ['var_1', 'var_2']  
     In[2]: %%stata -i example_list  
            display "`example_list'"
+           
+Retrieve user-defined macros from Stata into the following Python dictionary `macro_dict` (**New in 0.2**): 
 
-Prevents any output from being shown below the cell, used to run Stata code quietly:  
+    -gm --getmacro    
+    In[1]: %%stata -gm macro_1 -gm macro_2  
+           local macro_1 item1 item2
+           local macro_2 item3 item4
+    In[2]: macro_dict['macro_1']
+    In[3]: macro_dict['macro_2']
+    
+Prevents any output from being shown below the cell:  
 
     -np --noprint  
     In[1]: %%stata -np  
 
-For debugging or inspection purposes it is possible to force the Stata window to remain open after execution:
+For debugging or inspection purposes it is possible to open the Stata window instead of running it quietly:
 
     -os --openstata  
     In[1]: %%stata -os  
 
-Syntax Highlighting
-===================
+##Session manager 
+
+IPyStata 0.2 introduces the possibility to use many different Stata sessions that by default run in the background. In order to avoid using unnecessary system resources several tools and automatic cleanup routines are included.
+
+### Tools:
+ 
+Display all active Stata sessions:
+
+    In[1]: %%stata
+		   sessions
+Reveal all Stata sessions:
+
+    In[1]: %%stata
+		   reveal all		 
+Hide all Stata sessions:
+
+    In[1]: %%stata
+		   hide all		 
+Close all Stata sessions initiated by IPyStata:
+
+    In[1]: %%stata
+		   close
+Close all Stata sessions (**Warning! This closes <u>all</u> Stata windows**):
+
+    In[1]: %%stata
+		   close all
+### Automatic clean-up routines:
+
+- At import/load all Stata sessions initiated by a previous IPyStata import are terminated. 
+- When IPyStata is unloaded it tries to close all Stata sessions that it created. 
+ 
+##Syntax Highlighting
 
 Experimental support for Stata syntax highlighting is included. CodeMirror does not have a Stata mode, hence the R mode is modified to accomodate Stata code. Setup instructions are below:
 
@@ -110,12 +193,15 @@ Create a new folder in the "mode" folder called 'stata'
 
 Copy **stata.js** from the ipystata folder (see Github) into the newly created 'stata' folder.
 
-Special mentions
-================
+##Special mentions
+
 
 This project is inspired by and based on the excelent work of:  
-Rpy2 (https://pypi.python.org/pypi/rpy2) and Fortran Magic (https://pypi.python.org/pypi/fortran-magic)
 
-Disclaimer
-==========
+- Rpy2 (https://pypi.python.org/pypi/rpy2)
+- Fortran Magic (https://pypi.python.org/pypi/fortran-magic)
+- Stata-Kernel (https://github.com/jrfiedler/stata-kernel)
+
+##Disclaimer
+
 This project is not affiliated with or endorsed by Statacorp.
--- ipystata/ipystata_magic.py
+++ ipystata/ipystata_magic.py
@@ -11,28 +11,30 @@ from IPython.core.magic import (Magics, magics_class, line_magic,
                                 cell_magic, line_cell_magic, needs_local_scope)
 from IPython.core.magic_arguments import (argument, magic_arguments,
     parse_argstring)
-
 from IPython.core import display
-
 from IPython.utils.path import get_ipython_cache_dir
 
-from time import sleep
-
+import time
 import pandas as pd
 import sys
 import subprocess
 import os
 import re
+import win32com.client
+import psutil
+import atexit
 
 class iPyStata:
+
     def __init__(self):
         self._config_dir = os.path.join(get_ipython_cache_dir(), 'stata', 'config')
         self._config_file = os.path.join(self._config_dir, 'configuration.ini')
+        self._pid_file = os.path.join(self._config_dir, 'pid_list.txt')
+
         if py3:
             self.Config = configparser.ConfigParser()
         else:
             self.Config = ConfigParser.ConfigParser()
-        self.Config.read(self._config_file)
 
         if not os.path.exists(self._config_dir):
             os.makedirs(self._config_dir)
@@ -41,6 +43,32 @@ class iPyStata:
                 self.Config.add_section('Stata configuration')
                 self.Config.set('Stata configuration','installation',"C:\Program Files (x86)\Stata13\StataMP-64.exe")
                 self.Config.write(configfile)
+
+        self.Config.read(self._config_file)
+
+        ## Perform a clean-up at the start (Note: this closes all previously started win32com Stata instances!)
+
+        if os.path.isfile(self._pid_file):
+            with open(self._pid_file, 'r') as pid_text:
+                still_open = self.get_stata_pid()
+                pids = pid_text.read().split(',')
+                count_close = 0
+                for x in pids:
+                    if int(x) in still_open:
+                        psutil.Process(int(x)).terminate()
+                        count_close += 1
+                if count_close > 0:
+                    print('Terminated %d unattached Stata session(s).' % count_close)
+
+        time.sleep(1)
+
+        stata_dir = os.path.join(get_ipython_cache_dir(), 'stata')
+        for x in os.listdir(stata_dir):
+            if re.match('^log_.*', x):
+                try:
+                    os.remove(os.path.join(stata_dir, x))
+                except:
+                    pass
 
     def ConfigSectionMap(self, section):
         dict1 = {}
@@ -55,28 +83,49 @@ class iPyStata:
                 dict1[option] = None
         return dict1
 
-    def config_stata(self, stata):
-        self.Config.set('Stata configuration', 'Installation', r'%s' % stata)
-        with open(self._config_file, 'w') as configfile:
-            self.Config.write(configfile)
-        self.Config.read(self._config_file)
-
     def process_log(self, log):
-        with open(log) as f:
-            code = f.read()
-        code = re.sub("\\n\. ..*?\\n", "\n", code)
-        code = re.sub("\\n\.\s\\n", "\n", code)
-        code = re.sub("(\\n)\\1{2,}", "\n\n", code)
-        code = re.sub('\s*name: *.*?\\n', "\n", code)
-        code = re.sub('\s*log: *.*?\\n', "\n", code)
-        code = re.sub('\s*log type: *.*?\\n', "\n", code)
-        code = re.sub('\s*opened on: *.*?\\n', "\n", code)
-        code = re.sub('\s*closed on: *.*?\\n', "\n", code)
-        code = re.sub('^-*\\n', "", code)
-        code = re.sub('-*\\n$', "", code)
+        with open(log, 'r+') as log_file:
+            code = log_file.read()
+            code = re.sub('^-*\\n', "", code)
+            code = re.sub('-*\\n$', "", code)
+            code = re.sub('\s*name: *.*?\\n', "\n", code)
+            code = re.sub('\s*log: *.*?\\n', "\n", code)
+            code = re.sub('\s*log type: *.*?\\n', "\n", code)
+            code = re.sub('\s*opened on: *.*?\\n', "\n", code)
+            code = re.sub('\s*closed on: *.*?\\n', "\n", code)
+            code = re.sub('\s*closed on: *.*?\\n', "\n", code)
+            code = re.sub('\\n\\n. \n. ', '\\n\\n. ', code)
+            code = re.sub('\n\.\s\n', '\n', code)
+            code = re.sub("(?:\\n|^)\. ..*?\\n", "", code)
+            code = re.sub("\s{1,3}[0-9]{1,2}\.\s.*?\\n", "", code)
+            code = re.sub(r"\.\s\w.*?(?:\\n|$)", "", code)
+            code = re.sub('\\n{1,}$', "", code)
+            code = re.sub('^\\n{0,}', "\n", code)
+            log_file.truncate(0)
         return code
 
+    def get_stata_pid(self):
+        pid_list = []
+        for proc in psutil.process_iter():
+            try:
+                if re.match('stata', proc.name(), flags=re.I):
+                    pid_list.append(proc.pid)
+            except:
+                pass
+        return pid_list
+
 iPyStata = iPyStata()
+
+def clean_at_close():
+    if os.path.isfile(iPyStata._pid_file):
+        with open(iPyStata._pid_file, 'r') as pid_text:
+            still_open = iPyStata.get_stata_pid()
+            pids = pid_text.read().split(',')
+            for x in pids:
+                if int(x) in still_open:
+                    psutil.Process(int(x)).terminate()
+
+atexit.register(clean_at_close)
 
 @magics_class
 class iPyStataMagic(Magics):
@@ -84,8 +133,14 @@ class iPyStataMagic(Magics):
     def __init__(self, shell):
         super(iPyStataMagic, self).__init__(shell)
         self._lib_dir = os.path.join(get_ipython_cache_dir(), 'stata')
+        self._pid_file = iPyStata._pid_file
         if not os.path.exists(self._lib_dir):
             os.makedirs(self._lib_dir)
+
+        self.session_dict = {}
+        self.do_dict = {}
+        self.log_dict = {}
+        self.pid_list = []
 
     @magic_arguments()
 
@@ -94,16 +149,94 @@ class iPyStataMagic(Magics):
     @argument('-o', '--output', help='This is the output argument.')
     @argument('-np', '--noprint', action='store_true', default=False, help='Force the magic to not return an output.')
     @argument('-os', '--openstata', action='store_true', default=False, help='Open Stata.')
+    @argument('-s', '--session', default='main', help='The name of a Stata session in which the cell has to be executed.')
+    @argument('-cwd', '--changewd', action='store_true', default=False, help='Set the current Python working directory in Stata session.')
+    @argument('-gm', '--getmacro', action='append', help='This will attempt to output the named macro values as a dictionary.')
 
     @needs_local_scope
     @cell_magic
     def stata(self, line, cell, local_ns=None):
 
-        old_cwd = os.getcwdu() if sys.version_info[0] == 2 else os.getcwd()
-        os.chdir(self._lib_dir)
+        def active_check(session):
+            try:
+                self.session_dict[session].UtilIsStataFree()
+                return True
+            except:
+                return False
 
         def stata_list(l):
-            return ' '.join(l)
+            return ' '.join([str(x) for x in l])
+
+        ## Debug mode (Delete before pushing)
+
+        if re.match(r'^debug$', cell, flags=re.I):
+            args = parse_argstring(self.stata, line)
+            return args.getmacro
+
+        ## Support functions: sessions, close, close all, reveal all, hide all
+
+        if re.match(r'^sessions$', cell, flags=re.I):
+            print('The following sessions have been found:')
+            for z in self.session_dict.keys():
+                if active_check(z):
+                    print(z + ' [active]')
+                else:
+                    print(z + ' [not active]')
+            return
+
+        if re.match(r'^close$', cell, flags=re.I):
+            print('The following sessions have been closed:')
+            close_list = []
+            for z in self.session_dict.keys():
+                if active_check(z):
+                    self.do_dict[z]('exit')
+                    close_list.append(z)
+                    print(z)
+            for z in close_list:
+                self.session_dict.pop(z, None)
+            time.sleep(1)
+            if os.path.isfile(self._pid_file):
+                with open(self._pid_file, 'r') as pid_text:
+                    still_open = iPyStata.get_stata_pid()
+                    pids = pid_text.read().split(',')
+                    for x in pids:
+                        if int(x) in still_open:
+                            psutil.Process(int(x)).terminate()
+                            print('Terminated unattached Stata session.')
+            self.pid_list = []
+            return
+
+        if re.match(r'^close all$', cell, flags=re.I):
+            closed_count = 0
+            for x in iPyStata.get_stata_pid():
+                psutil.Process(x).terminate()
+                closed_count += 1
+            self.session_dict = {}
+            print('Terminated %d running Stata processes' % closed_count)
+            return
+
+        if re.match(r'^reveal all$', cell, flags=re.I):
+            reveal_count = 0
+            for x in self.session_dict.keys():
+                if active_check(x):
+                    self.session_dict[x].UtilShowStata(0)
+                    reveal_count += 1
+            print('Revealed %d Stata sessions.' % reveal_count)
+            return
+
+        if re.match(r'^hide all$', cell, flags=re.I):
+            hide_count = 0
+            for x in self.session_dict.keys():
+                if active_check(x):
+                    self.session_dict[x].UtilShowStata(1)
+                    hide_count += 1
+            print('%d Stata sessions have been hidden.' % hide_count)
+            return
+
+        ## Obtain the current working directory and if requested set as Stata working directory.
+
+        python_cwd = os.getcwdu() if sys.version_info[0] == 2 else os.getcwd()
+        #os.chdir(self._lib_dir)
 
         args = parse_argstring(self.stata, line)
 
@@ -128,6 +261,7 @@ class iPyStataMagic(Magics):
                 else:
                     pass
 
+        data_dir = os.path.join(self._lib_dir, 'data_input.dta')
         if args.data:
             data = args.data
             try:
@@ -146,56 +280,117 @@ class iPyStataMagic(Magics):
                     else:
                         pass
                 if date_var:
-                    val.to_stata(r'data_input.dta', convert_dates= {x:'tc' for x in date_var}, write_index=False)
+                    val.to_stata(data_dir, convert_dates= {x:'tc' for x in date_var}, write_index=False)
                 else:
-                    val.to_stata(r'data_input.dta', write_index=False)
+                    val.to_stata(data_dir, write_index=False)
             else:
                 pass
 
-        stata = iPyStata.ConfigSectionMap("Stata configuration")['installation']
-        tmp_path='.'
+        ## Check whether the Stata session exists and is active, status is stored.
 
-        with open("code.do", "w") as myfile:
-            myfile.write("clear" + "\n" + "set more off"+ "\n" + 'set linesize 200' + "\n")
-            for x,y in zip(name_list, stata_lists):
-                myfile.write("local "+ x + ' ' + y + "\n")
-            if args.data:
-                myfile.write(r'use "data_input.dta"'+"\n")
-            myfile.write('log using log_output, replace text' + '\n')
-            myfile.write('quietly cd "%s"'% old_cwd + '\n')
-            myfile.write(cell)
-            myfile.write('\n' + 'quietly cd "%s"' % self._lib_dir + '\n')
-            myfile.write('\n' + 'log close' + '\n')
-            if args.output:
-                myfile.write(r'saveold "data_output.dta", replace ' + '\n')
-            myfile.write("\n" + "// End of line deletion prevention")
+        session_id = args.session
+
+        if session_id in self.session_dict and isinstance(self.session_dict[session_id], win32com.client.CDispatch):
+            try:
+                self.session_dict[session_id].UtilIsStataFree()
+                active = True
+            except:
+                print('Please note: session %s was no longer active and is restarted.' % session_id)
+                active = False
+        else:
+            active = False
+
+        ## If Stata session does not exist or became inactive; start one and store the PID.
+
+        if not active:
+            pid_before = iPyStata.get_stata_pid()
+            time.sleep(0.5)
+            self.log_dict[session_id] = os.path.join(self._lib_dir, 'log_%s.txt' % session_id)
+            self.session_dict[session_id] = win32com.client.Dispatch("stata.StataOLEApp")
+            self.do_dict[session_id] = self.session_dict[session_id].DoCommandAsync
+            self.session_dict[session_id].UtilShowStata(1)
+            self.do_dict[session_id]('log using {} , text replace'.format(self.log_dict[session_id]))
+            self.do_dict[session_id]('set more off')
+
+            pid_after = iPyStata.get_stata_pid()
+            if len(pid_before) == 1 and pid_before == pid_after:
+                self.pid_list.append(pid_after[0])
+            else:
+                try:
+                    self.pid_list.append(list(set(pid_after) ^ set(pid_before))[0])
+                except:
+                    print('PID could not be obtained, can only be closed by using "close all".')
+
+        ## Send Stata session to foreground / background.
 
         if args.openstata:
-            cmd = [stata, "do", 'code.do']
+            self.session_dict[session_id].UtilShowStata(0)
         else:
-            cmd = [stata, '/e', "do", 'code.do']
+            self.session_dict[session_id].UtilShowStata(1)
 
-        try:
-            p = subprocess.Popen(cmd)
-            p.communicate()
-        except:
-            return "Failed to open Stata"
+        ## Build the command that is send to State.
+            ## Note, combination is to simplify log handling (only one log output has to be processed).
 
-        sleep(1)
-        out = iPyStata.process_log('log_output.log')
+        data_out = os.path.join(self._lib_dir, 'data_output.dta')
+        code_list = []
+        for x,y in zip(name_list, stata_lists):
+            code_list.append("local "+ x + ' ' + y + "\n")
+        if args.data:
+            code_list.append(r'use "%s"' % data_dir + "\n")
+        if args.changewd:
+            code_list.append('quietly cd "%s"'% python_cwd + '\n')
+            print('Set the working directory of Stata to: %s' % python_cwd)
+        code_list.append(cell)
+        if args.output:
+            code_list.append('quietly save "%s", replace ' % data_out + "\n")
+        code_txt= '\n'.join(code_list)
+
+        ## Execute code and wait for the Stata session to finish.
+
+        self.do_dict[session_id](code_txt)
+
+        while self.session_dict[session_id].UtilIsStataFree() == 0:
+            pass
+
+        ## Give the log file a second to save and then process it.
+
+        time.sleep(1)
+        out = iPyStata.process_log(self.log_dict[session_id])
 
         if args.output:
             try:
-                output_ipys = pd.read_stata(r'data_output.dta')
+                output_ipys = pd.read_stata(data_out)
                 try:
                     output_ipys.drop('index', axis=1, inplace=True)
                     self.shell.push({args.output: output_ipys })
                 except:
                     self.shell.push({args.output: output_ipys })
             except:
-                print("Exception has occured. File could not be loaded.")
+                print("Exception has occured. File could not be loaded. (Note, Pandas needs to be 0.17.x or higher.)")
 
-        os.chdir(old_cwd)
+        if args.getmacro:
+            macro_dict = {}
+            count_success = 0
+            fail_list = []
+            for x in args.getmacro:
+                try:
+                    temp_macro = self.session_dict[session_id].MacroValue("_%s" % x).split(' ')
+                    if temp_macro == ['']:
+                        fail_list.append(x)
+                    else:
+                        macro_dict[x] = temp_macro
+                        count_success += 1
+                except Exception as e:
+                    print(e)
+                    pass
+            if len(fail_list) > 0:
+                print("The following macros could not be found: %s" % ', '.join(fail_list))
+            if len(macro_dict.keys()) > 0:
+                self.shell.push({'macro_dict' : macro_dict})
+                print("Several (%dx) macros have been added to the dictionary: macro_dict" % count_success),
+
+        with open(self._pid_file, 'w') as pid_text:
+            pid_text.write(','.join(map(str, self.pid_list)))
 
         if not args.noprint:
             return print(out)
