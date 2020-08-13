e6c4f5ddf12162365b5de4b044ba7e2d3a70910c
Ties de Kok <TiesdeKok@users.noreply.github.com>
2020-08-13 14:00:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='ipystata',
-      version='0.4.0',
+      version='0.4.1',
       description='Enables the use of Stata together with Python via Jupyter (IPython) notebooks.',
       url='https://github.com/TiesdeKok/ipystata',
       author='TiesdeKok',
-      author_email='t.c.j.dekok@tilburguniversity.edu',
+      author_email='tdekok@uw.edu',
       license='MIT',
       packages=['ipystata'],
       zip_safe=False)
